8d5c03ab218f513446cb9ee519c414c5f6ba2c26
smarota17 <shrutimarota@gmail.com>
2022-11-10 16:53:05 -0500

    Initial email functionality

--- src/account.py
+++ src/account.py
@@ -1,13 +1,16 @@
 from account_info import AccountInfo
+from datetime import datetime, timedelta
+import smtplib, ssl
 
 class Account():
-    def __init__(self, name='', surname='', birthday='', interests='', wishlist='', friendlist='', ID = None):
+    def __init__(self, name='', surname='', birthday='', email='', interests='', wishlist='', friendlist='', ID = None):
         if ID != None:
             accountMan = AccountInfo()
             info = accountMan.get_info(ID)
             self.name = info['Name']
             self.surname = info['Surname']
             self.birthday = info['Birthday']
+            self.email = info['Email']
             self.interests = info['Interests']
             self.wishlist = info['WishList']
             self.friendlist = info['FriendList']
@@ -16,6 +19,7 @@ class Account():
             self.name = name
             self.surname = surname
             self.birthday = birthday
+            self.email = email
             self.interests = interests
             self.wishlist = wishlist
             self.friendlist = friendlist
@@ -24,23 +28,56 @@ class Account():
 
     def create_account(self):
         accountMan = AccountInfo()
-        acc = accountMan.create_account(self.name, self.surname, self.birthday, self.interests, self.wishlist, self.friendlist)
+        acc = accountMan.create_account(self.name, self.surname, self.birthday, self.email, self.interests, self.wishlist, self.friendlist)
         return acc
     
     def view_account(self):
         accountMan = AccountInfo()
         return accountMan.get_info(self.ID)
 
-    def update_account(self, name='', surname='', birthday='', interests='', wishlist='', friendlist=''):
+    def update_account(self, name='', surname='', birthday='', email='', interests='', wishlist='', friendlist=''):
             self.name = name
             self.surname = surname
             self.birthday = birthday
+            self.email = email
             self.interests = interests
             self.wishlist = wishlist
             self.friendlist = friendlist
             accountMan = AccountInfo()
-            accountMan.update_account(self.ID, name, surname, birthday, interests, wishlist, friendlist)
+            accountMan.update_account(self.ID, name, surname, birthday, email, interests, wishlist, friendlist)
 
+    def send_reminder_email(self):
+            accountMan = AccountInfo()
+            info = accountMan.get_info(self.ID)
+
+            name = info['Name']
+            birthday = info['Birthday']
+            email = info['Email']
+            friendlist = info['FriendList']
+            wishlist = info['WishList']
+
+            birthdayDate = datetime.datetime.strptime(birthday, "%Y-%m-%d").date()
+            currDate = datetime.datetime.now().date()
+            if (birthdayDate - currDate).days < 7: 
+                port = 587 
+                smtp_server = "outlook.office365.com"
+                sender_email = email
+                password = 'G1ft3r#212!'
+                for friend in friendlist:
+                    receiver_email = friend.email
+                    message = """\
+                    Subject: """ + name + """'s Birthday is Coming Up
+
+                    Buy """ + name + """ the perfect gift for their birthday on """ + birthday + """.
+                    Here are some items on their wishlist: """ + wishlist + """."""
+
+                    context = ssl.create_default_context()
+                    with smtplib.SMTP(smtp_server, port) as server:
+                        server.ehlo()  # Can be omitted
+                        server.starttls(context=context)
+                        server.ehlo()  # Can be omitted
+                        server.login(sender_email, password)
+                        server.sendmail(sender_email, receiver_email, message)
 
 
 # #acc = Account('Hannah', 'Montana', '05/05/1995', 'Singing, Dancing')
